fce3ca3ce97b919cf8672fe3c06c68e755450f46
alrwysa <alrwysa@gmail.com>
2021-03-07 02:12:15 +0300

    add signal, (auto create profile)

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import ungettext_lazy as _
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 class Profile(models.Model):
     '''
     already exist:
@@ -29,3 +30,11 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.user)
+
+    
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(
+            user = instance
+        )
