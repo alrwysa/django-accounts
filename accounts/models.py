955ccd6f0f5665737f9075328198f1daf9117a27
U-GOOGLE\Saudi <alrwysa@gmail.com>
2021-03-07 01:51:34 +0300

    add Profile with (user, phone number) field

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ungettext_lazy as _
 
-# Create your models here.
+class Profile(models.Model):
+    '''
+    already exist:
+    {
+        username
+        password
+        first name
+        last name
+        email
+    }
+    '''
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    #! show phone
+    phone_number = models.CharField(max_length=10, blank=True, null=True) #! change to False
+    #! region
+    #! city
+    #! type
+    #! major
+    #! rank
+    #! show desire
+    #! desire
+    #! text field for Notes
+    
+    
+
+    def __str__(self):
+        return str(self.user)
