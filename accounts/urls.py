c22f2f0dd0dac75ed1883d2d40a6062077717f14
alrwysa <alrwysa@gmail.com>
2021-03-07 12:56:01 +0300

    ad login with email or user

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name ='accounts'
 urlpatterns = [
     path('signup', views.signup, name='signup'),
-    path('profile', views.profile, name='profile'),
+    path('profile/', views.profile, name='profile'),
     path('profile/edit', views.profile_edit, name='profile_edit'),
 ]
